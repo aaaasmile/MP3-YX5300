16c344ba4e81b2ee64b55a4cb11595f5e3f92579
aaaasmile <testhaus@gmx.at>
2021-02-06 16:14:34 +0100

    start to play a song

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,182 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
-int Ledpin = LED_BUILTIN; // sul mio esp8266 è il pin marcato con D4, come mai?
-                          // nella documentazione di AZ-Delivery: Bei der Lolin V3 liegt sie allerdings an "GPIO2 / D4".
+#define ESP8266_RX 3 //D9 Rx should connect to TX of the Serial MP3 Player module
+#define ESP8266_TX 1 //D10 Tx connect to RX of the module
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(Ledpin, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-  Serial.begin(115200);
+static int8_t Send_buf[8] = {0};
+static uint8_t ansbuf[10] = {0};
+
+SoftwareSerial mp3(ESP8266_RX, ESP8266_TX);
+
+/************ Command byte **************************/
+#define CMD_NEXT_SONG 0X01 // Play next song.
+#define CMD_PREV_SONG 0X02 // Play previous song.
+#define CMD_PLAY_W_INDEX 0X03
+#define CMD_VOLUME_UP 0X04
+#define CMD_VOLUME_DOWN 0X05
+#define CMD_SET_VOLUME 0X06
+
+#define CMD_SNG_CYCL_PLAY 0X08 // Single Cycle Play.
+#define CMD_SEL_DEV 0X09
+#define CMD_SLEEP_MODE 0X0A
+#define CMD_WAKE_UP 0X0B
+#define CMD_RESET 0X0C
+#define CMD_PLAY 0X0D
+#define CMD_PAUSE 0X0E
+#define CMD_PLAY_FOLDER_FILE 0X0F
+
+#define CMD_STOP_PLAY 0X16 // Stop playing continuously.
+#define CMD_FOLDER_CYCLE 0X17
+#define CMD_SHUFFLE_PLAY 0x18  //
+#define CMD_SET_SNGL_CYCL 0X19 // Set single cycle.
+
+#define CMD_SET_DAC 0X1A
+#define DAC_ON 0X00
+#define DAC_OFF 0X01
+
+#define CMD_PLAY_W_VOL 0X22
+#define CMD_PLAYING_N 0x4C
+#define CMD_QUERY_STATUS 0x42
+#define CMD_QUERY_VOLUME 0x43
+#define CMD_QUERY_FLDR_TRACKS 0x4e
+#define CMD_QUERY_TOT_TRACKS 0x48
+#define CMD_QUERY_FLDR_COUNT 0x4f
+
+/************ Opitons **************************/
+#define DEV_TF 0X02
+
+String sbyte2hex(uint8_t b)
+{
+  String shex;
+
+  shex = "0X";
+
+  if (b < 16)
+    shex += "0";
+  shex += String(b, HEX);
+  shex += " ";
+  return shex;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print("Ciao ");
-  digitalWrite(Ledpin, HIGH);   // Turn the LED on (Note that LOW is the voltage level
-  // but actually the LED is on; this is because
-  // it is active low on the ESP-01)
+void sendCommand(byte command, byte dat1, byte dat2)
+{
+  delay(20);
+  Send_buf[0] = 0x7E;    //
+  Send_buf[1] = 0xFF;    //
+  Send_buf[2] = 0x06;    // Len
+  Send_buf[3] = command; //
+  Send_buf[4] = 0x01;    // 0x00 NO, 0x01 feedback
+  Send_buf[5] = dat1;    // datah
+  Send_buf[6] = dat2;    // datal
+  Send_buf[7] = 0xEF;    //
+  Serial.print("Sending: ");
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    mp3.write(Send_buf[i]);
+    Serial.print(sbyte2hex(Send_buf[i]));
+  }
+  Serial.println();
+}
+
+String sanswer(void)
+{
+  uint8_t i = 0;
+  String mp3answer = "";
+
+  // Get only 10 Bytes
+  while (mp3.available() && (i < 10))
+  {
+    uint8_t b = mp3.read();
+    ansbuf[i] = b;
+    i++;
+
+    mp3answer += sbyte2hex(b);
+  }
+
+  // if the answer format is correct.
+  if ((ansbuf[0] == 0x7E) && (ansbuf[9] == 0xEF))
+  {
+    return mp3answer;
+  }
+
+  return "???: " + mp3answer;
+}
+
+String decodeMP3Answer()
+{
+  String decodedMP3Answer = "";
+
+  decodedMP3Answer += sanswer();
+
+  switch (ansbuf[3])
+  {
+  case 0x3A:
+    decodedMP3Answer += " -> Memory card inserted.";
+    break;
+
+  case 0x3D:
+    decodedMP3Answer += " -> Completed play num " + String(ansbuf[6], DEC);
+    //sendCommand(CMD_NEXT_SONG);
+    //sendCommand(CMD_PLAYING_N); // ask for the number of file is playing
+    break;
+
+  case 0x40:
+    decodedMP3Answer += " -> Error";
+    break;
+
+  case 0x41:
+    decodedMP3Answer += " -> Data recived correctly. ";
+    break;
+
+  case 0x42:
+    decodedMP3Answer += " -> Status playing: " + String(ansbuf[6], DEC);
+    break;
+
+  case 0x48:
+    decodedMP3Answer += " -> File count: " + String(ansbuf[6], DEC);
+    break;
+
+  case 0x4C:
+    decodedMP3Answer += " -> Playing: " + String(ansbuf[6], DEC);
+    break;
+
+  case 0x4E:
+    decodedMP3Answer += " -> Folder file count: " + String(ansbuf[6], DEC);
+    break;
+
+  case 0x4F:
+    decodedMP3Answer += " -> Folder count: " + String(ansbuf[6], DEC);
+    break;
+  }
+
+  return decodedMP3Answer;
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  mp3.begin(9600);
+  //send the command [Select device] first. Serial MP3 Player
+  // only supports micro sd card, so you should send “ 7E FF 06 09 00 00 02 EF ”.
+  sendCommand(CMD_SEL_DEV, 0, DEV_TF);
   delay(500);
-  Serial.print("Mondo\n");
-  digitalWrite(Ledpin, LOW);  // Turn the LED off by making the voltage HIGH
-  delay(500);              
+}
+
+int g_first = true;
+void loop()
+{
+  if (g_first)
+  {
+    Serial.println("Wakeup, play the first song");
+    // 7E FF 06 0F 00 01 01 EF => Play the song with the directory:/01/001xxx.mp3
+    sendCommand(CMD_PLAY_FOLDER_FILE, 0x01, 0x03);
+    delay(3000);
+    g_first = false;
+  }
+  if (mp3.available())
+  {
+    Serial.println(decodeMP3Answer());
+  }
+  delay(100);
 }
